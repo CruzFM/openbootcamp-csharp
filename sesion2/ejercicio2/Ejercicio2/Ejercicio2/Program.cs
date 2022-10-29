76d0a208fc3247e7b8e365a387a4244e23a85578
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-10-29 15:47:00 -0300

    ejercicio 2 sesion 2 terminado.

---
+++ sesion2/ejercicio2/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Coche
+int puertas = 4;
+int ruedas = 4;
+string marca = "Volskwagen";
+string itv = "Si";
+
+//Mesa
+double peso = 3.5d;
+double largo = 2.2d;
+string material = "Madera";
+string color = "Rojo";
