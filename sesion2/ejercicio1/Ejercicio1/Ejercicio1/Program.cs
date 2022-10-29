d82316407fb055e7a6698ca5ae545b2781ed7fa8
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-10-29 15:13:31 -0300

    nuevo ejercicio 1 de sesion 2

---
+++ sesion2/ejercicio1/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Escriba por favor su nombre.");
+string name = Console.ReadLine();
+Console.WriteLine("Escriba su Apellido");
+string lastName = Console.ReadLine();
+Console.WriteLine("Ahora indique su edad.");
+string age = Console.ReadLine();
+Console.WriteLine("Usted sabe programar?");
+string canCode = Console.ReadLine();
+
+string answer = name + " " + lastName + ", " + age + " de edad, " + canCode;
+Console.WriteLine(answer);
